a95ebd62cb4a44da2f02d707384961e5fad5dbb2
Nikita Dudnik <nikdudnik@gmail.com>
2023-06-13 06:03:52 +0200

    Fix rendering from FBO

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ import vs from "./shaders/vs.vert?raw";
 import vs2 from "./shaders/vs2.vert?raw";
 import fs from "./shaders/fs.frag?raw";
 import fs2 from "./shaders/fs2.frag?raw";
-
 import uvGridUrl from "./assets/uvgrid.jpg";
-
-console.log(uvGridUrl);
 
 const App: Component = () => {
   let canvasEl: HTMLCanvasElement | undefined;
@@ -89,71 +86,66 @@ const App: Component = () => {
       const bufferInfo2 = twgl.createBufferInfoFromArrays(gl, arrays2);
 
 
-      // const videoFb = twgl.createFramebufferInfo(gl, [{format: gl.RGBA, type: gl.UNSIGNED_BYTE, min: gl.LINEAR, wrap: gl.CLAMP_TO_EDGE,}], 1920, 1080);
+      const videoFb = twgl.createFramebufferInfo(gl, undefined, 1920, 1080);
 
       const programInfo1 = twgl.createProgramInfo(gl, [vs, fs]);
       const programInfo2 = twgl.createProgramInfo(gl, [vs2, fs2]);
 
+      const videoTexture = twgl.createTexture(gl, {flipY: 1});
+
       function render(time: number) {
         if (gl === null) return;
         if (videoEl?.readyState === undefined || videoEl?.readyState < 2) return;
-        // twgl.bindFramebufferInfo(gl, videoFb);
 
-        // gl.clearColor(0, 0, 0, 0);
-        // gl.clear(gl.COLOR_BUFFER_BIT);
+        twgl.bindFramebufferInfo(gl, videoFb);
 
-        // const videoTexture = twgl.createTexture(gl, {
-        //   src: videoEl,
-        // });
-        // const uniforms = {
-        //   u_diffuse: videoTexture,
-        //   u_aspect: gl.canvas.width / gl.canvas.height,
-        //   u_time: time * 0.001,
-        // };
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        twgl.setTextureFromElement(gl, videoTexture, videoEl, {width: 1920, height: 1080});
+
+        const uniforms = {
+          u_diffuse: videoTexture,
+          u_aspect: gl.canvas.width / gl.canvas.height,
+          u_time: time * 0.001,
+        };
     
-        // gl.useProgram(programInfo1.program);
-        // twgl.setBuffersAndAttributes(gl, programInfo1, bufferInfo1);
-        // twgl.setUniforms(programInfo1, uniforms);
-        // twgl.drawBufferInfo(gl, bufferInfo1);
-      
+        gl.useProgram(programInfo1.program);
+        twgl.setBuffersAndAttributes(gl, programInfo1, bufferInfo1);
+        twgl.setUniforms(programInfo1, uniforms);
+        twgl.drawBufferInfo(gl, bufferInfo1);
 
-        // twgl.bindFramebufferInfo(gl, null);
-        // twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
-        // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-        // gl.clearColor(0, 0, 0, 0);
-        // gl.clear(gl.COLOR_BUFFER_BIT);
-        const fbTexture = twgl.createTexture(gl, {
-          src: uvGridUrl,
-        });
-        // const uniforms2 = {
-        //   u_diffuse: fbTexture
-        // };
-    
-        // gl.useProgram(programInfo2.program);
-        // twgl.setBuffersAndAttributes(gl, programInfo2, bufferInfo2);
-        // twgl.setUniforms(programInfo2, uniforms2);
-        // twgl.drawBufferInfo(gl, bufferInfo2);
-        
-
-
+        twgl.bindFramebufferInfo(gl, null);
 
         twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
         gl.clearColor(0, 0, 0, 0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        const uniforms = {
-          u_diffuse: fbTexture,
-          // time: time * 0.001,
-          // resolution: [gl.canvas.width, gl.canvas.height],
+        const uniforms2 = {
+          u_diffuse: videoFb.attachments[0],
         };
-
+    
         gl.useProgram(programInfo2.program);
         twgl.setBuffersAndAttributes(gl, programInfo2, bufferInfo2);
-        twgl.setUniforms(programInfo2, uniforms);
+        twgl.setUniforms(programInfo2, uniforms2);
         twgl.drawBufferInfo(gl, bufferInfo2);
+        
+        // twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
+        // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // gl.clearColor(0, 0, 0, 0);
+        // gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // const uniforms = {
+        //   u_diffuse: fbTexture,
+        // };
+
+        // gl.useProgram(programInfo2.program);
+        // twgl.setBuffersAndAttributes(gl, programInfo2, bufferInfo2);
+        // twgl.setUniforms(programInfo2, uniforms);
+        // twgl.drawBufferInfo(gl, bufferInfo2);
       }
 
       const [_, start] = createRAF(
